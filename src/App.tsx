d0c341e36b6407e70fed04d227fa67dc03aef265
Jacob Cohen <jacobcohen76@protonmail.com>
2023-02-07 11:03:36 -0700

    updated icon

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <img src="/logo.svg" className="logo react" alt="React logo" />
+      <img src="/studiohealthaz/logo.svg" className="logo react" alt="React logo" />
       <h1>Studio Health AZ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
