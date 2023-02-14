774c16e92fe5f6e2c2dcdea02baedb8f1d7710df
Jacob Cohen <jacobcohen76@protonmail.com>
2023-02-13 18:48:40 -0700

    changed w

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import StretchId from "@/components/StretchId"
 export default function App() {
   return (
     <div className="text-gray-50">
-      <img src="/studiohealthaz/logo.svg" className="content-center max-w-md" />
+      <img src="/studiohealthaz/logo.svg" className="w-48" />
       <h1>Studio Health AZ</h1>
       <p>WIP</p>
       <StretchId />
