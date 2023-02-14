4b303e83e2f854aed2bfe55a7956847cffdd5053
Jacob Cohen <jacobcohen76@protonmail.com>
2023-02-13 17:47:27 -0700

    switched to tailwind css

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import StretchId from "@/components/StretchId"
+
+export default function App() {
+  return (
+    <div className="text-gray-50">
+      <img src="/logo.svg" className="content-center max-w-md" />
+      <h1>Studio Health AZ</h1>
+      <p>WIP</p>
+      <StretchId />
+    </div>
+  )
+}
---
+++ src/components/StretchId.tsx
@@ -0,0 +1,22 @@
+
+export default function StretchId() {
+  return (
+    <>
+      <h1>Stretch your way to better health with Stretch i-D!</h1>
+      <p>
+        Stretch i-D is a hands-on muscle-imbalance identification technique,
+        integrated with advanced manual therapy for correction and realignment
+        of musculoskeletal imbalances. Stretch i-D includes:
+      </p>
+      <ol className='list-disc list-inside'>
+        <li>Posture Alignment</li>
+        <li>Trigger-Point Release</li>
+        <li>Myofascial Stretching</li>
+        <li>Physical Therapy</li>
+        <li>Stretch Therapy</li>
+        <li>Golf Conditioning</li>
+        <li>Metabolic Typing</li>
+      </ol>
+    </>
+  )
+}
